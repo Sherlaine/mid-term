572caa018a66df9fa08a4cc0fee48c0bae865a27
Sherlaine <sherlaine.lau@gmail.com>
2018-10-19 19:58:53 +0000

    google api add button feature

--- public/scripts/googlemaps.js
+++ public/scripts/googlemaps.js
@@ -1,14 +1,14 @@
 
-              // Global Variables. May need to refactor this code (refer to Spencer's explanation)
-              var searchLocation;
-              var map;
+// Global Variables. May need to refactor this code (refer to Spencer's explanation)
+let searchLocation;
+let map;
               // Initialize and add the map
                     function initMap() {
                       // The location of Uluru
                       const vancouver = {lat: 49.246292, lng: -123.116226};
                       // The map, centered at Uluru
-                      var map = new google.maps.Map(
-                          document.getElementById('map'), {zoom: 12, center: vancouver});
+                      map = new google.maps.Map(
+                          document.getElementById('map'), {zoom: 11, center: vancouver});
                       // The marker, positioned at Uluru
                       var marker = new google.maps.Marker({position: vancouver, map: map});
                       const doorIsOpen = {lat: 49.282622, lng: -123.095606};
@@ -24,7 +24,7 @@
             console.log('new search!!!!');
             var places   = searchBox.getPlaces();
             var location = places[0].geometry.location;
-
+            console.log(location)
 
             // Proof of latitude of longitude in console
             console.log('location lat', location.lat());
@@ -35,10 +35,13 @@
         });
                     }
                     // Marker adding functionality, but does not add marker to map
+                    
       function addMarker(lat, lng){
          myLatlng = new google.maps.LatLng(lat, lng);
+         console.log(myLatlng)
          // Will be able to add more details about marker (ex.'Hello PLUTO!'). Research documentation
          marker = new google.maps.Marker({position: myLatlng, map: map, title: 'Hello PLUTO!'});
+         console.log(marker, map)
       }
 
       // Function that posts marker to map when button is clicked
@@ -47,6 +50,7 @@
           // Proof that button works, will show in console
           console.log('will try to add point');
 
+          console.log(addMarker);
           // Adds marker when button is clicked
           addMarker(searchLocation[0], searchLocation[1]);
         })
