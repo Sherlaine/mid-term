bafd36b3bd40a4beee4a3cc3af5ef9d61f3ba568
Sherlaine <sherlaine.lau@gmail.com>
2018-10-22 03:25:06 +0000

    merge conflict fix

--- public/scripts/googlemaps.js
+++ public/scripts/googlemaps.js
@@ -10,7 +10,7 @@ function initMap() {
   };
   map = new google.maps.Map(
     document.getElementById('map'), {
-      zoom: 9,
+      zoom: 11,
       center: vancouver
     }
   );
@@ -37,15 +37,6 @@ function initMap() {
   });
 }
 
-
-
-// a location called Door Is Open to use to test marker functions below
-// replace with dynamic code
-const doorIsOpen = {
-  lat: 49.282622,
-  lng: -123.095606
-};
-
 // Function that posts marker to map when button is clicked
 // This Function works on the CLICKING OF "ADD BUTTON ----"
 $(function addMarkerToMap() {
@@ -60,121 +51,135 @@ $(function addMarkerToMap() {
     //IT ACTUALY PLACES THE MARKER ON THE MAP
     addMarker(searchLocation[0], searchLocation[1]);
   })
-  $('button#dumpster-heaven').on('click', function () {
-  })
-  
+
+  $('button#dumpster-heaven').on('click', function () {})
+
   $('button#vegan-janet').on('click', function () {
     janetDumpster()
   })
 
   $('button#east-van').on('click', function () {
     eastVanDumpster()
-
   })
 
-  $('#dumpster-heaven').on('click', (event) => {
-    let mapId = 1;
-    console.log('hi');
-    $.ajax('/maps/dumpsterHeaven/show', {
-      method: "GET",
-      data: {
-        map_id: mapId
-      },
-      success: (val) => {
-        let markers = val.markers;
-        markers.forEach((marker) => {
-          showMarker(marker.lat, marker.lng);
-        });
-      }
+})
 
-    })
-  })
-
-  // provides lat and long to add marker to addMarkerToMap function
-  function addMarker(lat, lng) {
-    console.log("clicked add button");
-    console.log("adding a marker at ", lat, lng);
-    // from Hafiz, test for the getserver function
-    sendMarkerToServer({
-      lat,
-      lng,
-      map_id: 1,
-      description: 'this is not real description'
-    });
-  }
-
-  function showMarker(lat, lng) {
-    var marker = new google.maps.Marker({
-      position: {
-        lat,
-        lng
-      },
-      map: map,
-      icon: 'https://media.giphy.com/media/udOUmtMweG2kg/giphy.gif',
-    });
-    allMyMarkers.push(marker);
-
-    marker.setMap(map)
-  }
-
-
-  function sendMarkerToServer(markerData) {
-    $.post('http://localhost:8080/api/markers', markerData)
-      .then(function (response) {
-        console.log(response)
-      })
-  }
-
-  //clears marker on current map
-  function clearMarkers() {
-    for (var i = 0; i < allMyMarkers.length; i++) {
-      allMyMarkers[i].setMap(null);
+$('#dumpster-heaven').on('click', (event) => {
+  let mapId = 1;
+  console.log('hi');
+  $.ajax('/maps/dumpsterHeaven/show', {
+    method: "GET",
+    data: {
+      map_id: mapId
+    },
+    success: (val) => {
+      let markers = val.markers;
+      markers.forEach((marker) => {
+        showMarker(marker.lat, marker.lng);
+      });
     }
-    console.log("is this working", clearMarkers)
-  }
 
-  //hard coded databases 
-  const doorIsOpen = {
-    lat: 49.282622,
-    lng: -123.095606
-  }
+  })
+})
 
-  const metrotown = {
-    lat: 49.2276257,
-    lng: -123.00757570000002
-  }
 
-  const kerrisdale = {
-    lat: 49.2331436,
-    lng: -123.15672689999997
-  }
+// provides lat and long to add marker to addMarkerToMap function
+function addMarker(lat, lng) {
+  console.log("clicked add button");
+  console.log("adding a marker at ", lat, lng);
+  // from Hafiz, test for the getserver function
+  sendMarkerToServer({
+    lat,
+    lng,
+    map_id: 1,
+    description: 'this is not real description'
+  });
 
-  const pne = {
-    lat: 49.28251499999999,
-    lng: -123.0429992
-  }
+  var marker = new google.maps.Marker({
+    position: {
+      lat,
+      lng
+    },
+    map: map,
+    icon: 'https://media.giphy.com/media/udOUmtMweG2kg/giphy.gif',
+  });
+  allMyMarkers.push(marker);
 
-  const ubc = {
-    lat: 49.26060520000001,
-    lng: -123.24599380000001
-  }
+  marker.setMap(map)
+}
 
-  const marpole = {
-    lat: 49.21072400000001,
-    lng: -123.13018699999998
-  }
+function showMarker(lat, lng) {
+  var marker = new google.maps.Marker({
+    position: {
+      lat,
+      lng
+    },
+    map: map,
+    icon: 'https://media.giphy.com/media/udOUmtMweG2kg/giphy.gif',
 
-  // to add marker when vegan janet button is clicked
-  function janetDumpster() {
-    console.log(doorIsOpen.lat, doorIsOpen.lng)
-    addMarker(doorIsOpen.lat, doorIsOpen.lng);
-    addMarker(metrotown.lat, metrotown.lng);
-    addMarker(kerrisdale.lat, kerrisdale.lng)
-  }
+  });
+  allMyMarkers.push(marker);
 
-  function eastVanDumpster() {
-    addMarker(pne.lat, pne.lng);
-    addMarker(ubc.lat, ubc.lng);
-    addMarker(marpole.lat, marpole.lng);
+  marker.setMap(map)
+}
+
+
+function sendMarkerToServer(markerData) {
+  $.post('http://localhost:8080/api/markers', markerData)
+    .then(function (response) {
+      console.log(response)
+    })
+}
+
+//clears marker on current map
+function clearMarkers() {
+  for (var i = 0; i < allMyMarkers.length; i++) {
+    allMyMarkers[i].setMap(null);
   }
-});
+  console.log("is this working", clearMarkers)
+}
+
+//hard coded databases 
+const doorIsOpen = {
+  lat: 49.282622,
+  lng: -123.095606
+}
+
+const metrotown = {
+  lat: 49.2276257,
+  lng: -123.00757570000002
+}
+
+const kerrisdale = {
+  lat: 49.2331436,
+  lng: -123.15672689999997
+}
+
+const pne = {
+  lat: 49.28251499999999,
+  lng: -123.0429992
+}
+
+const ubc = {
+  lat: 49.26060520000001,
+  lng: -123.24599380000001
+}
+
+const marpole = {
+  lat: 49.21072400000001,
+  lng: -123.13018699999998
+}
+
+// to add marker when vegan janet button is clicked
+function janetDumpster() {
+  console.log(doorIsOpen.lat, doorIsOpen.lng)
+  addMarker(doorIsOpen.lat, doorIsOpen.lng);
+  addMarker(metrotown.lat, metrotown.lng);
+  addMarker(kerrisdale.lat, kerrisdale.lng)
+}
+
+function eastVanDumpster() {
+  addMarker(pne.lat, pne.lng);
+  addMarker(ubc.lat, ubc.lng);
+  addMarker(marpole.lat, marpole.lng);
+}
